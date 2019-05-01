ce389b0f016d489b08270c22931b922e3e02ca5e
s17001 <s17001@std.it-college.ac.jp>
2019-05-02 00:46:56 +0900

    TSでのDOM操作やイベント追加のサンプルコードの追記

--- script/test.js
+++ script/test.js
@@ -1,6 +1,17 @@
+var colorList = ["red", "green", "blue", "black", "yellow"];
 var user = function (name) {
     return 'こんにちは、' + name + 'さん';
 };
 console.log(user('太郎'));
 var dom_h1 = document.getElementById("hello");
+var dom_click = document.getElementById("click");
 console.log(dom_h1);
+dom_h1.innerText = "令和になったなぁ";
+// dom_h1.style.color = "red"
+var click = function () {
+    console.log("clicked!!!!");
+    var dom_h1 = document.getElementById("hello");
+    var randnum = Math.floor(Math.random() * Math.floor(5));
+    dom_h1.style.color = colorList[randnum];
+};
+dom_click.addEventListener('click', click, false);
--- script/test.ts
+++ script/test.ts
@@ -1,9 +1,29 @@
+const colorList:string[] = ["red","green","blue","black","yellow"]
+
 let user = (name) => {
     return 'こんにちは、' + name + 'さん';
 }
 
 console.log(user('太郎'));
 
-const dom_h1:HTMLElement = document.getElementById("hello")
+let dom_h1:HTMLElement = document.getElementById("hello")
+let dom_click:HTMLElement = document.getElementById("click")
 
 console.log(dom_h1)
+
+dom_h1.innerText = "令和になったなぁ"
+// dom_h1.style.color = "red"
+
+const click = () => {
+    console.log("clicked!!!!");
+    
+    let dom_h1:HTMLElement = document.getElementById("hello")
+
+    let randnum:number = Math.floor(Math.random() * Math.floor(5))
+
+    dom_h1.style.color = colorList[randnum]
+
+}
+
+dom_click.addEventListener('click',click,false)
+
